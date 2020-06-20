dd54de54b161661f66f3a2fe4b713091992b8503
Otto <ottomwk@gmail.com>
2020-06-20 14:08:05 +0200

    Add example module and have it load in

---
+++ cogs/ping.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        """Gives you a good ole pong"""
+        ping = int(self.bot.latency*1000)
+        await ctx.send(f"Pong! ({ping} ms)")
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
